37ec57b463025f06549479883948b48efaf77cda
tomatesmp-crypto <tomatesmp@gmail.com>
2026-05-24 16:54:56 -0400

    fix: v2 - dev.nix with all bug fixes

--- dev.nix
+++ dev.nix
@@ -1,35 +1,22 @@
-# Zelin VPS - Firebase Studio
-# QEMU VM with SSH access + Cloudflare Tunnel
 { pkgs, ... }: {
   channel = "stable-24.05";
 
   packages = [
-    # QEMU - the core VM engine
     pkgs.qemu
-
-    # ISO creation for cloud-init
     pkgs.cdrtools
-
-    # noVNC dependencies
     pkgs.python3
     pkgs.python3Packages.websockify
-
-    # Network tools
     pkgs.curl
     pkgs.wget
     pkgs.git
     pkgs.openssh
-
-    # System utilities
     pkgs.htop
     pkgs.tmux
     pkgs.nano
     pkgs.jq
   ];
 
-  env = {
-    QEMU_SYSTEM_X86_64 = "${pkgs.qemu}/bin/qemu-system-x86_64";
-  };
+  env = {};
 
   idx = {
     extensions = [];
@@ -47,4 +34,3 @@
     };
   };
 }
-
